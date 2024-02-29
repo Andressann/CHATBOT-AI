1afa56111986f49c87ddca3657c05373538c1e95
zarrok <camilo_alb.velasquez@uao.edu.co>
2024-02-29 18:51:36 -0500

    backend environment
    Installing Spring environment, with Spring web, spring Data JPA, mysql driver, WebSocket,Spring security

---
+++ Backend/IA-backend/src/test/java/uao/IACoomeva/IAbackend/IaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uao.IACoomeva.IAbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
